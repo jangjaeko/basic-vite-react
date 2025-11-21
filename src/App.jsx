6f9fc75415979190eb377354763b0a5d516e62f2
unknown <jangjaeko45@gmail.com>
2025-11-20 22:33:16 -0800

    feat : state expample

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import "./App.css";
-import Header from "./component/Header";
+// import Header from "./component/Header";
 // import Main from "./component/Main";
 // import Footer from "./component/Footer";
-import Button from "./component/Button";
+// import Button from "./component/Button";
+import { useState } from "react";
 
 function App() {
-  const buttonProps = {
-    text: "Click Me",
-    color: "red",
-    a: 1,
-    b: 2,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("off");
 
   return (
     <>
-      <Button text={"mail"} color={"blue"} />
-      <Button {...buttonProps} />
-      <Button text={"click"}>
-        <Header />
-        <div> children</div>
-      </Button>
+      <div>
+        <h1> The light is {light} </h1>
+        <button
+          onClick={() => {
+            setLight(light === "on" ? "off" : "on");
+            // light = light === "on" ? "off" : "on"; => no re-render
+          }}
+        >
+          {light === "on" ? "Turn off" : "Turn on"}
+        </button>
+      </div>
+      <h1> {count}</h1>
+      {/* when click event re-render */}
+      <button onClick={() => setCount(count + 1)}> Increase </button>
+      <button onClick={() => setCount(count - 1)}> Decrease </button>
     </>
   );
 }
