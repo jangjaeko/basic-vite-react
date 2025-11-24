01d7dcefd7e7dfc714caae254845948a614b52c6
unknown <jangjaeko45@gmail.com>
2025-11-23 23:41:00 -0800

    style simple couter UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import "./App.css";
 // import Button from "./component/Button";
 import { useState } from "react";
 // import Register from "./component/Register";
-import HookExam from "./component/HookExam";
+// import HookExam from "./component/HookExam";
+import Viewer from "./component/Viewer";
+import Controller from "./component/Controller";
 
 function App() {
   return (
-    <>
-      <HookExam />
-    </>
+    <div className="App">
+      <h1> Simple Couter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
   );
 }
 
